fc4499ce049b01bf8dc8db6b7a75d858631c1ab4
Filip Shellbranch <filip.hellgren04@gmail.com>
2026-09-07 15:23:22 +0200

    fix: fix

--- Exercise_5/benchmark.sh
+++ Exercise_5/benchmark.sh
@@ -6,7 +6,7 @@ N_VALUES=(1 2 4 8 16 32)
 
 # Executable name and output log
 PROGRAM="./build/e5.out" # Replace with your compiled executable
-OUTPUT_LOG="Exersize_5/results.txt"
+OUTPUT_LOG="Exercise_5/results.txt"
 
 # Clear or initialize the results file
 echo "T,N,Real_Time_Sec" > "$OUTPUT_LOG"
